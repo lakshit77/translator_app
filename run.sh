8177b3a5ddb3f60dac94a7b9e21302945cd1c340
Lakshit Ukani <techsunami77@gmail.com>
2023-10-02 00:50:27 +0530

    changes

--- run.sh
+++
@@ -1,8 +0,0 @@
-# sudo docker login -u $REGISTRY_NAME -p $REGISTRY_PASS
-
-if [ sudo docker ps -aq ]
-then 
-    sudo docker ps -aq | sudo xargs docker stop | sudo xargs docker rm 
-fi
-
-sudo docker run -d -p 8000:8000 $IMAGE_NAME
